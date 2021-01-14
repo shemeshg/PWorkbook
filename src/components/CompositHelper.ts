604844222b6227f958e7705c4abdc85d209e2714
osx user <gili.shemesh@gmail.com>
2021-01-14 17:54:51 +0200

    commit

---
+++ src/components/CompositHelper.ts
@@ -0,0 +1,39 @@
+
+import {PRef, PWorkbook} from "./PWorkbook"
+import {Ref, computed, ref} from '@vue/composition-api'
+
+export class CompositHelper {
+  // eslint-disable-next-line 
+    ary: {s: PRef<any>, r: Ref}[] = [];
+
+    pw: PWorkbook;
+    symbl: symbol;
+
+    constructor(pw: PWorkbook,symbl: symbol){
+      this.pw = pw
+      this.symbl = symbl
+    }
+
+    getComputed<T>(storeVar: PRef<T>){
+      const tmpRef = ref(storeVar.ref) as unknown as Ref<T>
+      const comp =  computed({
+          get: ()=>{ return tmpRef.value},
+          set: (s) =>{storeVar.ref = s}
+        })
+      this.ary.push({s: storeVar, r: tmpRef})
+      return comp
+    }
+
+    onMounted(){
+      this.pw.setRefreshCallback(this.symbl, (ret) => {    
+        this.ary.forEach( (row)=>{
+          ret.c(row.s, row.r)
+        })          
+      })
+    }
+
+
+    onUnmounted() {
+      this.pw.unSetRefreshCallback(this.symbl)
+    }
+}
